a3d973206149f355dbb3bfadf04003524c0a9be9
Arkady <arkon.f2@gmail.com>
2020-03-18 18:20:52 +0300

    Storage location in json

--- src/main/java/my/nvinz/rest1/Data.java
+++ src/main/java/my/nvinz/rest1/Data.java
@@ -1,12 +1,24 @@
 package my.nvinz.rest1;
 
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+
+import java.io.FileReader;
+
 public class Data {
     private String latitude;
     private String longtitude;
 
     public Data() {
-        this.latitude = "55.675273";
-        this.longtitude = "37.857642";
+        try (FileReader reader = new FileReader("data.json")) {
+            JSONObject jsonData = (JSONObject) new JSONParser().parse(reader);
+            this.latitude = (String) jsonData.get("latitude");
+            this.longtitude = (String) jsonData.get("longtitude");
+        } catch (Exception e) {
+            this.latitude = "55.675273";
+            this.longtitude = "37.857642";
+            e.printStackTrace();
+        }
     }
 
     public Data(String latitude, String longtitude) {
--- src/main/java/my/nvinz/rest1/Rest1RestController.java
+++ src/main/java/my/nvinz/rest1/Rest1RestController.java
@@ -1,7 +1,11 @@
 package my.nvinz.rest1;
 
+import org.json.simple.JSONObject;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
+
+import java.io.FileWriter;
+import java.io.IOException;
 
 @CrossOrigin(maxAge = 3600)
 @RestController
@@ -17,5 +21,16 @@ public class Rest1RestController {
     @RequestMapping(value = "/getData", method = RequestMethod.POST/*, consumes = MediaType.TEXT_PLAIN_VALUE*/)
     public void setData(@RequestBody Data data) {
         this.data.setData(data);
+
+        JSONObject jsonData = new JSONObject();
+        jsonData.put("latitude", data.getLatitude());
+        jsonData.put("longtitude", data.getLongtitude());
+
+        try (FileWriter file = new FileWriter("data.json")) {
+            file.write(jsonData.toJSONString());
+            file.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
